3050e9a76cdd71b5ce7353880006a8223f0c9948
Frank1993 <hupengcheng1993@126.com>
2016-12-25 19:47:54 +0800

    finish the word_2_vec_me.py

--- word_2_vec_me.py
+++ word_2_vec_me.py
@@ -21,7 +21,6 @@ import zipfile
 
 import numpy as np
 
-from six.moves import xrange
 
 
 class DataAgent(object):
@@ -36,7 +35,7 @@ class DataAgent(object):
 		# data_index, indicate the word we are process
 		self.data_index = 0
 
-		self.sampledTable = self.getSampleTable()
+		#self.sampledTable = self.getSampleTable()
 
 	def extract_data(self,data_path,most_common_num):
 		with open(data_path,'r') as f:
@@ -73,7 +72,7 @@ class DataAgent(object):
 
 		return data,count,word_id_dict,id_word_dict
 
-	def gen_batch(self, batch_size,num_context_word_for_center_word,context_window):
+	def gen_batch(self,batch_size,num_context_word_for_center_word,context_window):
 		"""
 
 		we generate a batch every time, for example, "The cat jumped on the puddle", when "jumped" is the center word, then "cat"
@@ -94,19 +93,20 @@ class DataAgent(object):
 
 
 		batch = np.ndarray(shape=(batch_size),dtype = np.int32)
-		labels = np.ndarray(shape  = (batch_size,1), dtype = np.int32)
+		labels = np.ndarray(shape  = (batch_size), dtype = np.int32)
 
 
 		# the number of words will process in a context
 		span = 2 * context_window + 1
 
-		word_queue = deque(maxLen = span)
+		word_queue = deque(maxlen = span)
 
 		# init the queue
 		for _ in xrange(span):
 			word_queue.append(self.data[self.data_index])
 			self.data_index = (self.data_index + 1)%len(self.data)
 
+		#print(word_queue)
 		# now we can get the center word use word_queuep[context_window]
 
 		for i in xrange(batch_size // num_context_word_for_center_word):
@@ -123,10 +123,12 @@ class DataAgent(object):
 				batch[i * num_context_word_for_center_word + j] = word_queue[context_window]
 				labels[i * num_context_word_for_center_word + j] = word_queue[target]
 
-			word_queue.append(self.data_index)
+			word_queue.append(self.data[self.data_index])
+			#print(self.data_index)
+			#print(word_queue)
 			self.data_index = (self.data_index + 1) % len(self.data)
 
-			return batch,labels
+		return batch,labels
 
 
 	def getSampleTable(self):
@@ -184,7 +186,11 @@ class  Word2Vec(object):
 
 		self.data_agent = data_agent
 
-	def build_options():
+		self.build_options()
+
+		self.graph = self.build_graph()
+
+	def build_options(self):
 		self.vocabulary_size = 50000
 		self.batch_size = 128
 		self.embedding_size = 128
@@ -192,18 +198,94 @@ class  Word2Vec(object):
 		self.num_context_word_for_center_word = 2
 		self.num_negative_samples = 2
 
-	def build_graph():
-		with tf.Graph():
-			batch = tf.placeholder(tf.int32,shape = [self.batch_size])
+	def add_placeholder(self):
+		self.batch = tf.placeholder(tf.int32,shape = [self.batch_size])
 
-			# we compress the word in the context and negative words together
-			labels = tf.placeholder(tf.int32,shape = [self.batch_size,1])
+		# we compress the word in the context and negative words together
+		self.labels = tf.placeholder(tf.int32,shape = [self.batch_size])
+
+	def build_graph(self):
+		graph = tf.Graph()
+		with graph.as_default():
+			self.add_placeholder()
 
 			input_vectors = tf.Variable(tf.truncated_normal([self.vocabulary_size,self.embedding_size]))
 
 			output_vectors = tf.Variable(tf.truncated_normal([self.vocabulary_size,self.embedding_size]))
 
-			input_embed = tf.nn.embedding_lookup(input_vectors)
+			input_embed = tf.nn.embedding_lookup(input_vectors,self.batch)
+
+			target_embed = tf.nn.embedding_lookup(output_vectors,self.labels)
+
+
+			neg_sampled_ids,_,_ = tf.nn.fixed_unigram_candidate_sampler(
+				true_classes = tf.reshape(tf.cast(self.labels,tf.int64),[self.batch_size,1]),
+				num_true = 1,
+				num_sampled = self.num_negative_samples,
+				unique = True,
+				range_max = self.vocabulary_size,
+				distortion = 0.75,
+				unigrams = [word_count[1] for word_count in self.data_agent.count])
+
+			neg_sampled_embed = tf.nn.embedding_lookup(output_vectors,neg_sampled_ids)
+
+			true_logits = tf.reduce_sum(tf.mul(input_embed,target_embed),axis =1)
+
+
+			# we use the same negative samples for every sample in a batch
+			neg_logits = tf.matmul(input_embed,neg_sampled_embed,transpose_b = True)
+
+
+			true_ten = tf.nn.sigmoid_cross_entropy_with_logits(true_logits,tf.ones_like(true_logits))
+
+			neg_ten = tf.nn.sigmoid_cross_entropy_with_logits(neg_logits,tf.zeros_like(neg_logits))
+			
+			#self.loss = tf.reduce_sum(true_ten)/self.batch_size
+
+			self.loss = (tf.reduce_sum(true_ten)
+			        + tf.reduce_sum(neg_ten))/self.batch_size
+
+			optimizer = tf.train.GradientDescentOptimizer(0.01)
+
+			self.train_op = optimizer.minimize(self.loss)
+
+		return graph
+
+	def train(self):
+		with self.graph.as_default():
+			init = tf.global_variables_initializer()
+
+			with tf.Session() as sess:
+				sess.run(init)
+
+				for i in range(100001):
+					batch,labels = self.data_agent.gen_batch(self.batch_size,self.num_context_word_for_center_word,self.context_window)
+					loss,_= sess.run([self.loss,self.train_op],feed_dict = {self.batch:batch,self.labels:labels})
+					if i % 10000 == 0:
+						print("step:%s loss:%s"%(i,loss))
+
+
+
+if __name__ == "__main__":
+	da = DataAgent(r"/Users/hu/development/tensorflow/tensorflow_word_2_vec/text8",50000)
+	#batch,label =  da.gen_batch(128,2,1)
+
+	#print( max(da.data))
+	#print(len(da.count))
+	#print(batch)
+	#print(label)
+	#print(batch)
+	#print(label)
+	wordVec = Word2Vec(da)
+	wordVec.train()
+
+
+
+
+
+
+
+
 
 
 
